2742dc3db1a1ed80fb0cf9f3b0f434461d40024b
Daniel Morandini <danielmorandini@me.com>
2020-09-17 17:53:14 +0200

    Add Spawner interface & World definition

---
+++ spawner.go
@@ -0,0 +1,30 @@
+package spawner
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+)
+
+type World struct {
+	Id      string `json:"id"`
+	Addr    string `json:"addr"`
+	Spawner string `json:"spawner"`
+
+	Details json.RawMessage `json:"details"`
+}
+
+type Spawner interface {
+	// Name of this spawner.
+	Name() string
+	// Spawn starts a World. Once the function returns, World should
+	// be ready to accept connections on World.Addr.
+	Spawn(ctx context.Context, r io.Reader) (*World, error)
+	// Kill stops & purges a previously spawned World.
+	Kill(ctx context.Context, w World) error
+	// Ps lists the currently running Worlds.
+	Ps(ctx context.Context) ([]World, error)
+}
+
+func DecodeWorld(w *World, r io.Reader) error         { return json.NewDecoder(r).Decode(w) }
+func EncodeWorlds(w io.Writer, worlds ...World) error { return json.NewEncoder(w).Encode(w) }
